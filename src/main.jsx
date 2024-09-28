ceadfd8da350aa2b7628102f3135cca27d6cf689
Marc Seiferle <marc.seiferle@bluewin.ch>
2024-09-28 18:35:04 +0200

    add jotai state mgmt

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { DevTools } from 'jotai-devtools'
+import 'jotai-devtools/styles.css'
+
 import { App } from './App.jsx'
 
 import './styles/main.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <DevTools />
     <App />
   </StrictMode>
 )
