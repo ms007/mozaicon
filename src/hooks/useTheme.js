af5bb16583446fa0caafc97a80bf259a4c37213f
Marc Seiferle <marc.seiferle@bluewin.ch>
2024-10-03 12:25:32 +0200

    get themeing user preference

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -12,8 +12,23 @@ const colors = {
   dark,
 }
 
-export const useTheme = (initialTheme = 'light') => {
-  const [name, setName] = useLocalStorage('theme', initialTheme)
+const setClassName = (theme) => {
+  document.body.classList.remove('light', 'dark')
+  document.body.classList.add(theme)
+}
+
+const getInitialColorMode = () => {
+  const mql = window.matchMedia('(prefers-color-scheme: dark)')
+  const hasMediaQueryPreference = typeof mql.matches === 'boolean'
+  if (hasMediaQueryPreference) {
+    return mql.matches ? 'dark' : 'light'
+  }
+
+  return 'light'
+}
+
+export const useTheme = () => {
+  const [name, setName] = useLocalStorage('theme', getInitialColorMode())
   const [theme, setTheme] = useAtom(presetsThemeAtom)
   const [, setGlobalVariables] = useGlobalVariables()
 
@@ -21,6 +36,7 @@ export const useTheme = (initialTheme = 'light') => {
     const variables = colors[name]
     setGlobalVariables(variables)
     setTheme(name)
+    setClassName(name)
   }, [name, setTheme, setGlobalVariables])
 
   const toggleTheme = () => {
